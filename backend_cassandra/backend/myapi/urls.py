f3d0d3429dccc289f3500d7e5cfd0c4a631a2fbd
fedevita <federico.vita1997@gmail.com>
2021-09-21 22:10:03 +0200

    fix

--- backend_cassandra/backend/myapi/urls.py
+++ backend_cassandra/backend/myapi/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from myapi.views import *
 from django.urls import path
 urlpatterns = [
-    path('users/<str:username>/',get),
+    path('users/<str:username>/',getUser),
 ]
